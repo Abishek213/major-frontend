49a8fedff7b47b5969fa04e28491b4932d9532d2
Hemant Ojha <hemant.ojha021@apexcollege.edu.np>
2025-11-16 19:51:14 +0545

    minor changes un about jsx

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -2,11 +2,12 @@ import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Calendar, Users, MapPin, Trophy, Check, ArrowRight } from 'lucide-react';
 import { useTheme } from '../context/ThemeContext';
-import eventImage from '../assets/images/event.webp';
-import communityImage from '../assets/images/community.webp';
-import locationImage from '../assets/images/location.jpg';
-import premiumImage from '../assets/images/premium.jpg';
-import bookingImage from '../assets/images/booking.jpg';
+import eventImage from '../../assets/images/event.webp';
+import communityImage from '../../assets/images/community.webp';
+import locationImage from '../../assets/images/location.jpg';
+import premiumImage from '../../assets/images/premium.jpg';
+import bookingImage from '../../assets/images/booking.jpg';
+
 
 const About = () => {
   const { isDarkMode } = useTheme();
